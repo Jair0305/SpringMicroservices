ab11ff3fd0eeea6ea459cd10382372ab3cb0d5ec
Jair0305 <chamaco_03@hotmail.es>
2023-12-16 01:06:46 -0600

    Added logic for student and course entities

---
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.microservice.student.controller;
+
+import com.microservice.student.entities.Student;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    public void save(@RequestBody Student student) {
+        studentService.save(student);
+    }
+
+    @GetMapping("/all")
+    ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(studentService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id)
+    {
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+}
